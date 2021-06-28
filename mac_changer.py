f1608f3177a25099dedbe3883f1cde525076c514
OzkanInonlu <79935049+OzkanInonlu@users.noreply.github.com>
2021-06-28 12:47:40 +0300

    Add files via upload
    Change your virtual machine's MAC Address using Python

---
+++ mac_changer.py
@@ -0,0 +1,26 @@
+import subprocess as sb
+import optparse as opt
+import time
+
+def get_inputs():
+    p_object = opt.OptionParser()
+    p_object.add_option("-i", "--int", dest="interface")
+    p_object.add_option("-m", "--m", dest="mac_address")
+    return p_object.parse_args()
+ # tuple içinde bir dictionary bir de empty list var, 1. dict, 2. empty list
+
+
+def change_mac(interface, mac_address):
+    sb.call(["ifconfig", interface, "down"])
+    sb.call(["ifconfig", interface, "hw", "ether", mac_address])
+    sb.call(["ifconfig", interface, "up"])
+    time.sleep(2)
+    print("Success")
+    time.sleep(2)
+    sb.call(["ifconfig", "eth0"])
+
+
+(user_inputs, arg) = get_inputs()
+interface=user_inputs.interface
+mac_address=user_inputs.mac_address
+change_mac(interface,mac_address)
